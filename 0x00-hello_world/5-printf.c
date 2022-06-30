8e7be518a5f554d83fca712a1dc32a3bb6b69da8
Chepngeno-langat <langatkarenn@gmail.com>
2022-06-30 11:53:48 -0700

    Task 5 - c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
  * main - prints string
- * Return: 0 when compiled correctl
- */          
+ * Return: 0 when compiled properly
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
