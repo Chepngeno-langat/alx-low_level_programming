4bf7509964159dae04d82e21378190e37ea5558a
Chepngeno-langat <langatkarenn@gmail.com>
2022-06-30 07:20:32 -0700

    Task 4 - c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * c code
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
