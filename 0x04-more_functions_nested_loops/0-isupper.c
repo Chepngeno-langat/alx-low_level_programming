421ea6fb3aa485b10ad271949283cdf31c9c51b1
Chepngeno-langat <langatkarenn@gmail.com>
2022-07-07 02:44:04 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: parameter to be checked
+ * Return: 0 or 1 according to condition
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+{
+return (0);
+}
+}
