f283dca286e97c06e77c2aeeb0ba0e1e0a8827df
Chepngeno-langat <langatkarenn@gmail.com>
2022-07-11 14:48:57 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every chracter on a string
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+int len, i;
+
+len = strlen(str);
+for (i = 0; i < len; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
