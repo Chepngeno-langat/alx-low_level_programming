89101959deebc839ecbbb3f921d3f928da7f2532
Chepngeno-langat <langatkarenn@gmail.com>
2022-07-14 06:49:00 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts to uppercase
+ * @x: parameter
+ * Return: returns in uppercase
+ */
+char *string_toupper(char *x)
+{
+return (toupper(x));
+}
