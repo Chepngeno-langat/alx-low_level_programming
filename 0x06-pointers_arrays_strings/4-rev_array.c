5316a4292de54be16310d7f9eb7a19940b42eac8
Chepngeno-langat <langatkarenn@gmail.com>
2022-07-14 06:32:55 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,12 @@ void reverse_array(int *a, int n)
 {
 int i;
 int temp;
-int arr[n];
 
 for (i = 0; i < n / 2; i++)
 {
-temp = arr[i];
-arr[i] = arr[n - i - 1];
-arr[n - i - 1] = temp;
+temp = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = temp;
 }
-return (arr[i]);
+return (a[i]);
 }
