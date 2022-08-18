724fa136eae5159d467c4c7044f882f9a52c946c
Chepngeno-langat <langatkarenn@gmail.com>
2022-08-18 23:11:03 +0300

    Task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+/**
+ * function that converts a binary number to an unsigned int
+ * @b
+ *
+ * return: the converted number or 0
+ */
+
+/*int _pow(int x,int y)
+{
+if(y < 0)
+{
+return(-1);
+}
+else if (y == 0)
+{
+return(1);
+}
+else
+{
+y--;
+x = x* _pow(x,y);
+return(x);
+}
+return(0);
+}*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int dec = 0;
+/*int i, x, n;*/
+int i = 0; rem;
+
+if (b == NULL)
+{
+return (0);
+}
+
+n = strlen(b) - 1;
+while (n != 0)
+{
+rem = n % 10;
+n /= 10;
+dec += rem * pow(2, i);
+++i;
+}
+/*for (x = 0, i = 0; n >= x; n--)
+{
+switch (b[i])
+{
+case '1':
+sum = sum + _pow(2, n);
+i++;
+break;
+case '0':
+sum = sum + 0;
+i++;
+break;
+default:
+return (0);
+}
+}*/
+
+return (sum);
+}
